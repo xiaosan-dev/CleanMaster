f90ccbcc7a759e69a1a62b112c38050f29b1920b
xiaolong.wang <xiaolong.wang@fenrir-inc.com.cn>
2026-06-09 15:14:44 +0800

    fix: clean up Compose build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,10 +37,6 @@ android {
     buildFeatures {
         compose = true
     }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
-    }
 }
 
 dependencies {
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -18,6 +19,10 @@ android {
 
     kotlinOptions {
         jvmTarget = "17"
+    }
+
+    buildFeatures {
+        compose = true
     }
 }
 
